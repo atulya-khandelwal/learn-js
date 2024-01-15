d884cbf87c27efe7d14687957923a26b51880ecc
atulya-khandelwal <154871009+atulya-khandelwal@users.noreply.github.com>
2024-01-15 16:16:25 +0000

    underastand concept of conversion from one to anothetr

---
+++ 01_basics/conversion.js
@@ -0,0 +1,50 @@
+// let score = NaN
+
+//can be used both methods
+// console.log(typeof score);
+// console.log(typeof(score));
+
+//conversion  Number   String   Boolean   Symbol
+
+// valueInNumber=  Boolean(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+/* any to number 
+
+"33"=>33
+"33dk" =>NaN
+null =>0
+true =>1
+false => 0
+undefined=>NaN
+
+*/
+
+/* any to boolean
+
+0 =>true
+1=> false
+"dfsd"=> true
+""=>false
+undefined, null, NaN=>false
+
+*/
+
+
+
+
+// *********** operations**********
+
+
+// let str1="atulya ";
+// let str2="khandelwal";
+// let str3= str1+str2;
+// console.log(str3);
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+
+// console.log(+true, -true, +false, -false);  =>output 1,-1,0,-0
+
+// console.log(+"", -"", +"dsfd", "-dsfsd")   =>0 ,-0, NaN, NaN     
