4a236ee62fe57d4aefff1e6042775466d10dfd42
atulya-khandelwal <154871009+atulya-khandelwal@users.noreply.github.com>
2024-01-15 16:19:03 +0000

    understand math object

---
+++ 01_basics/math.js
@@ -0,0 +1,13 @@
+/*
+
+    round, ceil, floor, trunc
+    squrt, cbrt
+    and some other properties
+
+*/    
+
+// console.log(Math.floor((Math.random()*10)+1))   =>print randomly generate values from 1 to 10
+
+// generate random values from 41 to 50
+// (Math.floor(Math.random() * (max - min + 1) + min))   =>this is the formula
+
