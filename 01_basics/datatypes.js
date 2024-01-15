f84e1f6c158a4345fe172c5dcc04a6b42ed34e19
atulya-khandelwal <154871009+atulya-khandelwal@users.noreply.github.com>
2024-01-15 16:15:06 +0000

    to understand the concept of datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict";  //treate all js code as a newer version
+
+// alert(5+6);        // we are using node js not browser
+
+let name="atulya";
+let no=8965;
+let isLogged=true;
+
+/*
+
+        *************Data Types****************
+number-> range 2 to power 53;   use big_int when number is out of range
+string ->use '' or ""
+boolean ->true or false
+null -> standalone value
+undefined
+symbol -> when we talk about uniqueness
+object
+*/
+
+//for find the type of variables ->
+console.log(typeof null);   // =>output is output
+
+/*
+//two types of data types
+1. primitive  (memory in stack & pass by value)
+2. non - primitive (memory in heap & pass by reference)
+primitive : number , string, boolean, undefined, symbol, null, BigInt
+non-primitive: object and array and function
+
+*/
