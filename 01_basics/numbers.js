d7a8cfccaaadccaca37ba0a65d36ef21d4e655bf
atulya-khandelwal <154871009+atulya-khandelwal@users.noreply.github.com>
2024-01-15 16:18:24 +0000

    understand number object

---
+++ 01_basics/numbers.js
@@ -0,0 +1,30 @@
+let value= new Number(450.155);
+
+// console.log(typeof value);   =>output-> object
+
+//to convert into string
+// console.log(value.toString());
+
+//to convert value into integer value by rounding off
+//return value is string
+//can be given a parameter. poitn ke bad kitni value chahiye
+// console.log(value.toFixed(4))
+
+// let num = 5.123456;
+
+// console.log(num.toPrecision()); // '5.123456'
+// console.log(num.toPrecision(5)); // '5.1235'
+// console.log(num.toPrecision(2)); // '5.1'
+// console.log(num.toPrecision(1)); // '5'
+
+// num = 0.000123;
+
+// console.log(num.toPrecision()); // '0.000123'
+// console.log(num.toPrecision(5)); // '0.00012300'
+// console.log(num.toPrecision(2)); // '0.00012'
+// console.log(num.toPrecision(1)); // '0.0001'
+
+// // note that exponential notation might be returned in some circumstances
+// console.log((1234.5).toPrecision(2)); // '1.2e+3'
+
+
