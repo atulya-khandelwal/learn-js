b47cafad3012ea10c311f164242767d6e9f45818
atulya-khandelwal <154871009+atulya-khandelwal@users.noreply.github.com>
2024-01-15 16:17:34 +0000

    understand the concept of strings

---
+++ 01_basics/string.js
@@ -0,0 +1,31 @@
+// let value = new String("dss,dfds,dfw,erf,ss");
+// console.log(value);
+
+// to find the character at particular index otherwise empty string(type string)
+// console.log(typeof value.charAt(3));
+
+// give character code at that particular index otherwise NaN(number)
+// console.log(typeof value.charCodeAt(3));
+
+//to replace that particular part
+// console.log(value.replace('ds','kd'))
+
+//to remove white spaces from beggning and end of the string
+// console.log(value.trim());
+
+// console.log(value.toUpperCase())
+// console.log(value.toLowerCase())
+
+//includes method return true if given string is found otherwise false
+// console.log(value.includes('ssd'))
+
+// split that string by given value and give array of values
+// console.log(value.split('d'));
+
+// find index of that particular string otherwise -1
+// console.log(value.indexOf("dss"))   console.log(value.lastIndexOf("Dfs"))
+//can be give second parameter for tell from when should start
+
+// first parameter:start index  second parameter: end index(exclude)
+// console.log(value.substring(1,6))
+// console.log(value.slice(-10,6));
