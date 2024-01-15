1155ed63a39c24d990738d2e2f19895d076c61a3
atulya-khandelwal <154871009+atulya-khandelwal@users.noreply.github.com>
2024-01-15 16:20:14 +0000

    understand date object

---
+++ 01_basics/date.js
@@ -0,0 +1,46 @@
+// let date=new Date()
+
+// console.log(date)
+// console.log(date.toLocaleString())
+// console.log(date.toLocaleDateString())
+// console.log(date.toLocaleTimeString())
+// console.log(date.toString())
+
+
+// let date=new Date("2024-11-02")             YYYY-MM-DD format
+// let date2=new Date("01-11-2024")               MM-DD-YYYY format
+
+
+// let date= new Date(year month day hour minutes seconds milliseconds)
+
+
+// let date =new Date()
+// console.log(date);
+// console.log(date.toISOString());
+// console.log(date.toUTCString());
+// console.log(date.toDateString());
+// console.log(date.toJSON());
+
+// let date= new Date();
+// console.log(date.getTime())
+// console.log(date.getFullYear());
+// console.log(date.getMonth())
+// console.log(date.getDate())
+// console.log(date.getDay())
+// console.log(date.getHours())
+// console.log(date.getMinutes())
+// console.log(date.getSeconds())
+// console.log(date.getMilliseconds())
+// console.log(date.getTimezoneOffset())
+
+
+// let date = new Date
+// console.log(date.setTime())
+// console.log(date.setFullYear(2023));
+// console.log(date.setMonth(2))
+// console.log(date.setDate(24))
+// console.log(date.setHours(15))
+// console.log(date.setMinutes(36))
+// console.log(date.setSeconds(15))
+// console.log(date.setMilliseconds(526))
+
