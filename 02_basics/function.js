f3ab793e22066463d822831c82b9905144c1938b
atulya-khandelwal <154871009+atulya-khandelwal@users.noreply.github.com>
2024-01-16 05:53:38 +0000

    understand functions

---
+++ 02_basics/function.js
@@ -0,0 +1,59 @@
+// function add(num1, num2){
+//     return num1+num2;
+// }
+
+// function()=>{
+//    
+// }
+
+//implicit return 
+    // const result= (num1 , num2)=> num1+num2
+// for return object use ()
+
+// console.log(add(5,6));
+
+// function addNum(...num1){               //rest operatr(...)
+//     return num1
+// }
+
+// console.log(addNum(100,5,10,302))
+
+// function addNum(val1,val2,...num){
+//     return num
+// }
+// console.log(addNum(100,200,300,400))
+
+// can be passed array,object,function in aruments
+
+// addOne(5)                                       //don't give an error               
+// function addOne(num){
+//     return num+1;
+// }
+
+// addTwo(5);                                      //give an error
+// const addTwo = function(num){
+//     return num +2
+// }
+
+
+
+
+//this keyword
+// use to point current context
+
+// if outside in browser give window object
+// if use in arrow function or outside give an empty Object    
+// in other type of function =>give an Object
+
+
+
+
+
+// Immediately Invoked Function Expressin(IIFE)
+
+// global scope ki problem ko hatane ke lie
+// immediately execute krae ke lie
+
+// ((name)=>{
+//     console.log("rsg", name);
+// })("kjk");                       // please use semicolon to end this execution
