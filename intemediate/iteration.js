e087bc35f796bab745480b4cc0a2ed592ade1cfc
atulya-khandelwal <154871009+atulya-khandelwal@users.noreply.github.com>
2024-01-16 15:15:25 +0000

    iterations

---
+++ intemediate/iteration.js
@@ -0,0 +1,70 @@
+//for loop
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+//break and continue
+
+
+// while loop
+// while (condition) {
+    
+// }
+
+
+//do-while
+
+// do {
+    
+// } while (condition);
+
+
+// for of loop                  object iterable nhi h
+// const arr = [1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+
+
+//Map=>used to store key-value pair
+
+// const map =new Map()
+// map.set("IN","India");
+// map.set("US", "USA");
+// map.set("FR","France")
+
+// for([key,value] of map){
+//     console.log(key, ': ', value)
+// }
+
+
+
+//for in loop   map iterable nhi h
+//  const myObject = {
+//     cpp : "c++",
+//     rb : "ruby",
+//     swift : "swift by apple"
+//  }
+
+//  for(key in myObject){
+//     console.log(myObject[key])
+//  }
+
+
+//for Each loop
+
+// const coding=["java", "python","cpp","ruby","javascript"]
+
+// coding.forEach(function(value, index, array){
+//     console.log(`value is: ${value}`)
+// } )
+
+// function printMe(value){
+//     console.log(value)
+// }
+
+// coding.forEach(printMe);
