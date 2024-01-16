6e82ed7f3614f08bf73d46f3830bf2af2a1f91a4
atulya-khandelwal <154871009+atulya-khandelwal@users.noreply.github.com>
2024-01-16 13:19:00 +0000

    understand control flow concept

---
+++ intemediate/controlFlow.js
@@ -0,0 +1,51 @@
+// falsy values => false,0,-0,BigInt 0n, null, undefined, empty string(""),NaN
+//truthy values => "0", "false", " ", [], {}, function(){} and everything
+
+
+//Nullish Coalescing Operator (??) : null and undefined
+
+// let val1;
+// val1=5??10          //output 5
+// val1=null??10       //output 10
+// val1=undefined??15``//output 15
+// val1=null??10??15   //output 10
+
+
+
+//Teranary operator
+
+//condition ? true : false
+
+
+
+
+// if(ture){                             // comparison operator=>
+//                                 //  >, <, <=, >=, ==, !=, ===, !==
+// }
+// else{
+
+// }
+
+// if(ture) console.log("ture1"), console.log("true2");
+
+
+// Nested if else
+
+
+//if(true&&true)
+
+// switch (key) {
+//     case value:
+//                             //to copy something ctr + shift + down or up arrow
+//         break;
+//     case value:
+        
+//         break;
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+//         break;
+// }
